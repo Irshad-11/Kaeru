661a33b7d850c9d9d0763658f2f291c5334af8b4
Irshad <irshadrisad@gmail.com>
2026-04-05 22:49:15 +0600

    Turn off Local Debugger

--- app.py
+++ app.py
@@ -1593,5 +1593,5 @@ def debug_schema():
 
 init_db()
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True, port=5000)
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', debug=True, port=5000)
